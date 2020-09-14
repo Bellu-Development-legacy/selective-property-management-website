3ac841453dfd2fcd08f5286682a9ba785165923e
Andrew <abellucci@bellu.dev>
2020-09-14 00:09:56 -0400

    v1

--- src/api/data.js
+++ src/api/data.js
@@ -23,37 +23,37 @@ export default {
         },
         {
             title: "Properties",
-            subheading: "",
+            subheading: "We focus on providing individualized and personalized service, developing a management program specifically for each property we manage.  We work to ensure our goals are in line with our clients goals. When choosing SPM, you invest in us so we invest in you. We work hard to build trusting relationships and strive to maintain the respect of everyone we deal with. We maintain a strong code of ethics.",
             route: "/properties",
             ghostId: "5f5e5e2fd82b524bf1df9e9d"
         },
         {
             title: "Administration",
-            subheading: "",
+            subheading: "Administrative services are a critical component of Selective Property Management’s comprehensive approach. The management of a property is performed under the directives of an Association’s Board. We stress being responsive and reliable in time of need. Our 24-hour emergency service enables us to respond to your needs in a very expedient manner. Our good working relationship with legal firms well versed in condominium law are also a strong benefit for Associations managed by SPM.",
             route: "/services/administration",
             ghostId: "5f5e62cfd82b524bf1df9f5a"
         },
         {
             title: "Accounting & Reporting",
-            subheading: "",
+            subheading: "Our aggressive, yet sensible, assessment fee collection policy is also an aide to an Association; and each Association has separate financial accounts that are independent from any other Association or SPM.",
             route: "/services/accounting-reporting",
             ghostId: "5f5e6328d82b524bf1df9f63"
         },
         {
             title: "Maintenance",
-            subheading: "",
+            subheading: "Our maintenance capabilities are limitless. A full complement of insured landscapers, snow removal firms, and insured, trained technicians for physical plant operations (i.e., pump stations, ejector stations, sewerage, electrical, plumbing, sprinkler systems, pool, etc.) all operating as sub-contractors is available. We feel our sub-contractors are the best in the State and the most reasonable in price. We feel our experience in attaining the lowest quality prices possible in all facets of the business could definitely be beneficial to your Association. At any time, Board members may have contact and/or consultations with the sub-contractors and referrals regarding sub-contractors are always welcomed from the Association. Board members always have final approval on all firms utilized and on all invoices for projects done.",
             route: "/services/maintenance",
             ghostId: "5f5e6341d82b524bf1df9f6a"
         },
         {
             title: "Homeowner's Associations",
-            subheading: "",
+            subheading: "Our property managers understand the unique demands of serving HOA and condominium associations. Over the years, Selective Property Management has developed a formula that works for Associations, Boards, and Homeowners, resulting in a favorable experience for everyone.",
             route: "/services/homeowners-associations",
             ghostId: "5f5e636dd82b524bf1df9f70"
         },
         {
             title: "Condominium Management",
-            subheading: "",
+            subheading: "For more than 30 years, Selective Property Management has provided quality services to hundreds of units and homes across Rhode Island, Massachusetts and even a part of New Hampshire. Our portfolio of unique communities ensures that Selective Property Management is prepared to help you handle all your management needs. We know how to work with Boards and Associations of all sizes; and we can provide your organization with individual needs and superior customer service.",
             route: "/services/condominium-management",
             ghostId: "5f5e638cd82b524bf1df9f77"
         },
@@ -65,33 +65,33 @@ export default {
         },
         {
             title: "Rules & Regulations Request",
-            subheading: "",
+            subheading: "Please complete and submit this form to register a Rules & Regulations Request. Your Property Manager will contact you to arrange for pick up. There is no fee for this request.",
             route: "/forms/rules-regulations",
-            ghostId: ""
+            ghostId: "5f5e8ba2d82b524bf1df9f8b"
         },
         {
             title: "Condominium Documents Request",
-            subheading: "",
+            subheading: "Please complete and submit this form to register a Condominium Documents Request. Your Property Manager will contact you to arrange for payment of the required fee. Documents are released once payment has been received.",
             route: "/forms/condominium-documents",
-            ghostId: ""
+            ghostId: "5f5ee514d39f8b04c36eff5f"
         },
         {
             title: "Insurance Certificate Request",
-            subheading: "",
+            subheading: "Please complete and submit this form to register an Insurance Certificate Request.",
             route: "/forms/insurance-certificate",
-            ghostId: ""
+            ghostId: "5f5ee8bcd39f8b04c36eff74"
         },
         {
             title: "Submit Maintenance Request",
-            subheading: "",
+            subheading: "Please complete and submit this form to file a Maintenance Request.",
             route: "/forms/submit-maintenance",
-            ghostId: ""
+            ghostId: "5f5ee5e7d39f8b04c36eff65"
         },
         {
             title: "Resale Certificate Request",
-            subheading: "",
+            subheading: "Please complete and submit this form to ask for a Resale Certificate Request. Please allow 7-10 days for completion of this certificate. Your Property Manager will contact you in order to arrange for payment of the required fee. Documents will be released when payment is received. A resale certificate is only good until the end of the month in which it is issued. This means that if your closing date gets delayed, a new Resale Certificate will need to be issued.",
             route: "/forms/resale-certificate",
-            ghostId: ""
+            ghostId: "5f5edf9bd39f8b04c36eff48"
         },
     ]
 };
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -9,12 +9,14 @@ export default function Contact() {
         <h3>Reach out</h3>
         <div className="contact-map">
           <div className="contact-box">
-            <form action="">
+            <form action="http://localhost:3000/forms" method="POST">
               <div className="fields">
-                <input type="text" placeholder="Full Name" />
-                <input type="email" name="" id="" placeholder="Email Address" />
+                <input type="hidden" className="g-recaptcha-response" name="g-recaptcha-response" />
+                <input type="hidden" name="action" value="validate_captcha" />
+                <input type="text" name="fullName" placeholder="Full Name" />
+                <input type="email" name="emailAddress" id="" placeholder="Email Address" />
                 <textarea
-                  name=""
+                  name="message"
                   cols="30"
                   rows="10"
                   placeholder="Message"
